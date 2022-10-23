785522cf53d12f1f430cee9403bf81f1bd30b0e6
U SAI KIRAN <102009466+kiranuppara@users.noreply.github.com>
2022-10-23 11:08:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    define('TITLE',"Home | Complete Registertion From");
+    include 'includes/header.php';
+?>
+<div id="philosophy">
+    <hr>
+    <br><br>
+    <h1>Advanced Login and Registration Application</h1>
+    <br><br>
+    <p> Let us walk you through Complete Login and Registration Application using PHP and MySQL</p>
+    
+    <br><br><br>
+    
+</div>
+
+<?php 
+    include 'includes/footer.php';
+?>
